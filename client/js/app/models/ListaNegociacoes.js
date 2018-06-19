e214a25dfd69b96a1985fcd32e90e213a1ec6dfb
didiraja <didiraja@hotmail.com>
2018-06-19 11:51:10 -0300

    II: Capítulo 01 concluído

--- client/js/app/models/ListaNegociacoes.js
+++ client/js/app/models/ListaNegociacoes.js
@@ -1,15 +1,18 @@
 class ListaNegociacoes {
 
-    constructor(armadilha) {
+    constructor(contexto, armadilha) {
 
         this._negociacoes = [];
         this._armadilha = armadilha;
+        this._contexto = contexto;
 
     }
 
     adiciona(negociacao) {
         this._negociacoes.push(negociacao);
         this._armadilha(this);
+
+        // Reflect.apply(this._armadilha, this._contexto, [this]);
     }
 
     get negociacoes() {
@@ -20,6 +23,8 @@ class ListaNegociacoes {
     esvazia() {
         
         this._negociacoes = [];
+        this._armadilha(this);
+        // Reflect.apply(this._armadilha, this._contexto, [this]);
     }
 
 }
